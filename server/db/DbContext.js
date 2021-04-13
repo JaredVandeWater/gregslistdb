6f84cd3532f42170077354bb5853db0d1141e76b
NiceJared <jaredvande@gmail.com>
2021-04-13 14:38:57 -0600

    first commit

---
+++ server/db/DbContext.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import CarSchema from "../models/Car";
+import HouseSchema from "../models/House";
+import JobSchema from "../models/Job";
+
+
+// this is where models get registered and access from the db
+class DbContext {
+  Cars = mongoose.model("Car", CarSchema);
+  Houses = mongoose.model("House", HouseSchema);
+  Jobs = mongoose.model("Job", JobSchema);
+}
+
+export const dbContext = new DbContext();
